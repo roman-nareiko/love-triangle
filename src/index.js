1d1facc19e79eed670e6c507bf0398fc323d506d
Raman Nareika <Raman.Nareika@thomsonreuters.com>
2018-02-22 17:43:33 +0300

    impl govnokod

--- src/index.js
+++ src/index.js
@@ -3,5 +3,18 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  //[2, 3, 1, 5, 6, 4], n - k
+  let count = 0;
+
+  preferences.forEach((v, i) => {
+    let a = i + 1;  //1
+    let b = preferences[i]; //2
+    let c = preferences[b - 1]; //3
+    
+    if(a !== b && b !== c && a === preferences[c - 1]) {
+      count++;
+    }
+  });
+
+  return count/3;
 };
